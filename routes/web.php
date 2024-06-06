9d49a6e03c12955a4ed3f4c1ce99da9bca0cb908
shivam datta purkayastha <shivamdatta465@gmail.com>
2024-06-06 22:42:06 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+
+Route::get('/', function () {
+    return view('layout');
+});
+Route::resource('/students',StudentController::class);
+
+
